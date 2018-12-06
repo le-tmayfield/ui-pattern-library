fe9fa8457ee4cce3c57b8ec9b5b7c55cda78196c
Tom Mayfield <tmayfield@lizearle.com>
2018-12-06 16:20:34 +0000

    -Copying across node modules, hence massive commit -Initial project structure and gulp file

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    pug = require('gulp-pug'),
+    sass = require('gulp-sass');
+
+// config variables
+var config = {
+    deploy: './public',
+    origin: './origin',
+};
+
+// compile sass --> css
+gulp.task('sass', function(){
+    return gulp.src(config.origin + '/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(config.deploy + '/css'));
+});
+
+// compile pug --> html
+gulp.task('pug', function(){
+    return gulp.src([
+        config.origin + '/pug/**/*.pug',
+        '!' + config.origin + '/pug/templates/**',
+        '!' + config.origin + '/pug/includes/**',
+    ])
+    .pipe(pug({pretty:true}))
+    .pipe(gulp.dest(config.deploy))
+});
+
+//---------------------//
+//insert new tasks here//
+//---------------------//
+
+// log reports
+gulp.task('notification', function(done){
+    gutil.log('Gulp is running and local server has been set to port 3000');
+    done();
+});
+
+// run default task
+gulp.task('default', gulp.series('sass','pug','notification'));
